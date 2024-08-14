38cb496fc9022267973f3ad66f887f8a35029701
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-08-14 23:34:02 +0700

    update test file

--- apps/react-app/src/app/app.spec.tsx
+++ apps/react-app/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/hello world/i)).toBeTruthy();
+    expect(getByText(/react app/i)).toBeTruthy();
   });
 });
