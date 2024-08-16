d70927a056d4afc20149255a8a619728a716d944
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-08-16 16:33:33 +0700

    test

--- apps/react-app/src/app/app.tsx
+++ apps/react-app/src/app/app.tsx
@@ -4,7 +4,6 @@ export function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-blue-500">React App</h1>
-      <h1 className="text-3xl font-bold text-blue-500">React App2</h1>
 
       <Button variant="outline">Click me</Button>
     </div>
