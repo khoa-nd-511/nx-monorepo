84365b0ba7ff39170dc0c24d7a87ac8f9952d2c7
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-08-19 20:52:51 +0700

    remove test

--- apps/react-app/src/app/app.tsx
+++ apps/react-app/src/app/app.tsx
@@ -2,7 +2,6 @@ export function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-blue-500">React App</h1>
-      <h1 className="text-3xl font-bold text-blue-500">test</h1>
     </div>
   );
 }
